056693b7a2bb0e16eb4252743b19a908d8a67e7e
Diego Barcellos <diegocb81@gmail.com>
2021-02-01 23:38:02 -0300

    Desenvolvendo o menu e submenu

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+<head>
+    <title>Projeto Menu Responsivo</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <h2>Logomarca</h2>
+            </div><!--logo-->
+            <nav class="menu-desktop">
+                <ul>
+                    <li>
+                        <a href="">Home</a>
+                        <ul class="submenu-desktop">
+                            <li><a href="">Sessão 1</a></li>
+                            <li><a href="">Sessão 2</a></li>
+                            <li><a href="">Sessão 3</a></li>
+                        </ul><!--submenu-desktop-->
+                    </li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div><!--clear-->
+        </div><!--container-->
+    </header>
+</body>
+
+</html>
