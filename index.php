f9d3ce6e44f46681bcb4a1277d2f8cddfbceca21
diegocb81 <diegocb81@gmail.com>
2021-02-11 20:15:24 -0300

    Adicionando compatibilidade com IE e Edge

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Projeto Menu Responsivo</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/fontawesome.min.css">
     <link rel="stylesheet" href="css/all.css">
