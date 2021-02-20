6a1590766fafd4520bf0d5669518f9686360360d
diegocb81 <diegocb81@gmail.com>
2021-02-19 23:59:42 -0300

    Adicionando imagem

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
     <title>Projeto Menu Responsivo</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/fontawesome.min.css">
     <link rel="stylesheet" href="css/all.css">
@@ -90,6 +89,7 @@
             <h2>Nossa história</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis enim et sodales molestie. Duis ac odio vitae risus cursus convallis. Praesent nec tellus velit. Donec faucibus arcu sit amet nibh dictum, a cursus mi porttitor. Proin eget nulla nibh. Aenean justo diam, feugiat ultrices elementum porttitor, ornare at dui. Nunc auctor nibh vitae velit efficitur, eget sollicitudin ipsum pellentesque. Donec purus tellus, mattis eu sapien vitae, viverra scelerisque velit. Sed mi libero, interdum in dolor eget, pulvinar commodo nibh.</p>
         </div><!--nossa_historia-->
+        <img src="img/ezequiel-garrido-UkhbQE0mR8Q-unsplash.jpg" alt="Uruguay">
     </div><!--conteudo-->
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script type="text/javascript">
